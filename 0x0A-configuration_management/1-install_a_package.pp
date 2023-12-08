34e66cdbb5fea7f55cc17640130fe9d523ef1bd0
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-12-08 17:15:34 -0500

    web debugging

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,5 +5,5 @@ package { 'python-pip': ensure=>present }
 exec { 'install_flask':
     command=> '/usr/bin/pip3 install Flask==2.1.0',
     path=> ['/usr/bin']
-    require=> package['python3-pip'],
+    require => package['python3-pip'],
 }
